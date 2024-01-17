eac46c2a3bbb0dc7e6661fa791be4a489038ae72
bKoZii <bigkonkamon@gmail.com>
2024-01-17 21:18:29 +0700

    Removed API key [bruh]

--- src/env/env.example.ts
+++ src/env/env.example.ts
@@ -1,9 +1,9 @@
 export const env = {
     production: true,
     firebase: {
-      apiKey: 'AIzaSyCwIlw-8iDpyrA-QfI_PIe9i9esrRj5p8U',
+      apiKey: 'xxx',
       authDomain: 'cloud-konkamon.firebaseapp.com',
-      databaseURL: 'https://cloud-konkamon-default-rtdb.asia-southeast1.firebasedatabase.app/',
+      databaseURL: 'xxxxxxxx',
       projectId: 'cloud-konkamon',
       storageBucket: 'cloud-konkamon.appspot.com',
       messagingSenderId: 'xxx',
