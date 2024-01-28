87fb66716786451d42593fc5790f36447a5dbefb
bKoZii <bigkonkamon@gmail.com>
2024-01-28 12:26:01 +0700

    Re-content

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit {
-  title: 'Project'
-  constructor(
-  ) { }
-  ngOnInit(): void {
-  }
+export class ProjectComponent {
 
 }
