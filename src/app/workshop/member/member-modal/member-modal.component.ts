c0bc00c4c07dd16cd0c52975bbdb93b2e4629a10
bKoZii <bigkonkamon@gmail.com>
2024-01-29 19:02:53 +0700

    Updated Member[Added Firebase]

--- src/app/workshop/member/member-modal/member-modal.component.ts
+++ src/app/workshop/member/member-modal/member-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { IMember } from 'src/app/model/member.model';
 import { MemberService } from 'src/app/services/member.service';
 declare var Swal: any;
 @Component({
@@ -8,35 +9,42 @@ declare var Swal: any;
 
 })
 
-export class MemberModalComponent implements OnInit{
-  member = {
-    title: 'นาย',
-    fName: '',
-    lName: '',
-    email: '',
-    mobileNum: '',
-    lineID: '',
-    address: ''
-  }
-  mService = new MemberService;
+export class MemberModalComponent implements OnInit {
+
+  member: IMember = new IMember();
+  // mModel = new IMember;
   @Output()
   onClose = new EventEmitter();
-  constructor() { }
+  constructor(private mService: MemberService) {
+    this.member.title = "นาย";
+  }
   ngOnInit() { }
   closeModal() { this.onClose.emit(null); }
 
   submitForm() {
-    this.mService.submitData(this.member);
-    Swal.fire({
-      icon: 'success',
-      title: 'บันทึกข้อมูลสำเร็จ!',
-      text: this.member.title + ' ' + this.member.fName + ' ' + this.member.lName, 
-      showConfirmButton: false,
-      timer: 2000
-    }).then(() => {
-      this.closeModal();
+    this.mService.submitData(this.member).then(() => {
+      Swal.fire({
+        icon: 'success',
+        title: 'บันทึกข้อมูลสำเร็จ!',
+        text: this.member.title + ' ' + this.member.fName + ' ' + this.member.lName,
+        showConfirmButton: false,
+        timer: 2000
+      }).then(() => {
+        this.closeModal();
+      });
     });
+    // this.mService.submitData(this.member).then(() => {
+    //   Swal.fire({
+    //     icon: 'success',
+    //     text: 'บันทึกข้อมูลเรียบร้อยแล้ว!',
+    //     showConfirmButton: false,
+    //     timer: 1500
+    //   }).then(() => {
+    //     this.clearForm();
+    //   });
+    // });
   }
+
   clearForm() {
     this.member.address = '';
     this.member.title = 'นาย';
--- src/app/workshop/member/member.component.ts
+++ src/app/workshop/member/member.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { MemberModalComponent } from './member-modal/member-modal.component';
-declare var Swal: any;
-
+import { map } from 'rxjs/operators';
+import { IMember } from 'src/app/model/member.model';
+import { MemberService } from 'src/app/services/member.service';
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
@@ -9,9 +9,27 @@ declare var Swal: any;
 })
 
 export class MemberComponent {
-  
+
+  member: IMember[];
+  mModel = new IMember;
+  constructor(private mService: MemberService) {
+    this.retrieveData();
+  }
+  memberList: any;
   showModal: boolean = false;
   selectItem() {
     this.showModal = true;
+  }
+  retrieveData(): void {
+    this.mService.retrieveData().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      )
+    ).subscribe(data => {
+      this.memberList = data;
+      console.log(this.memberList);
+    });
   }
 }
